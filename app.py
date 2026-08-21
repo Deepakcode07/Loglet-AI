434089a7b83a73943be7b97b3836be76f5af28f7
Deepakcode07 <deepakkumardas2205@gmail.com>
2026-08-21 23:20:07 +0530

    working with gunicorn

--- app.py
+++ app.py
@@ -105,4 +105,6 @@ def edit_report():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
